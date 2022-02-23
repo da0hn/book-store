277445b0eeafd1cdb04007e6ecbf029ed3d7909f
da0hn <gabrielhondacba@gmail.com>
2022-02-23 18:02:28 -0400

    feat: add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './components/views/home/home.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -19,6 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +31,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [ AppComponent ],
