816322b848f1e3a8d30897feba6c76b923df8542
da0hn <gabrielhondacba@gmail.com>
2022-02-24 16:01:00 -0400

    feat: add route to create new category

--- src/app/components/views/categories/categories-routing.module.ts
+++ src/app/components/views/categories/categories-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './category-list';
+import { CategoryNewComponent } from './category-new/category-new.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CategoryListComponent,
+  },
+  {
+    path: 'new',
+    component: CategoryNewComponent,
   },
 ];
 
