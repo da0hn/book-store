3a15014d06e2d4c276c4f4716f953a461d28946b
da0hn <gabrielhondacba@gmail.com>
2022-03-02 10:40:57 -0400

    feat: add book list component

--- src/app/components/views/books/books-routing.module.ts
+++ src/app/components/views/books/books-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from '@book-store/views/books/book-list/book-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: undefined
-  }
+    component: BookListComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ],
 })
-export class BooksRoutingModule { }
+export class BooksRoutingModule {
+}
--- src/app/components/views/books/books.module.ts
+++ src/app/components/views/books/books.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
+import { BookListComponent } from './book-list/book-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookListComponent
+  ],
   imports: [
     CommonModule,
     BooksRoutingModule
