bcb2e918237dcd66ac555a406e1e3c57343affd0
da0hn <gabrielhondacba@gmail.com>
2022-03-03 15:15:13 -0400

    feat: implements all CRUD operations

--- src/app/components/views/books/shared/book.service.ts
+++ src/app/components/views/books/shared/book.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { BookTableView } from '@book-store/views/books';
+import { Book, BookTableView } from '@book-store/views/books';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +16,22 @@ export class BookService {
 
   findAllByCategory(idCategory: number): Observable<BookTableView[]> {
     return this.http.get<BookTableView[]>(`${this.baseUrl}/books?category=${idCategory}`);
+  }
+
+  findById(idCategory: number): Observable<Book> {
+    return this.http.get<Book>(`${this.baseUrl}/books/${idCategory}`);
+  }
+
+  save(newBook: Book): Observable<Book> {
+    return this.http.post<Book>(`${this.baseUrl}/books`, newBook);
+  }
+
+  deleteById(idBook: number): Observable<Book> {
+    return this.http.delete<Book>(`${this.baseUrl}/books/${idBook}`);
+  }
+
+  update(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.baseUrl}/books/${book.id}`, book);
   }
 
 }
